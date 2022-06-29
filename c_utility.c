6a53acfcb2c13951e6c99db9e4af29c7d940a3db
chunmingYang <95987545+chunmingYang@users.noreply.github.com>
2022-06-29 17:20:26 -0500

    Update c_utility.c

--- c_utility.c
+++ c_utility.c
@@ -1,1 +1,28 @@
-
+/*
+	how to save csv file in C
+*/
+// global initialization
+FILE *fid;
+char parh[]="/path/to/save/file"
+// data saving initialization (adding labels) function
+void init_save_data()
+{
+	fptintf(fid, "x1, x2, x3");
+	fprintf(fid, "x4, x5, x6");
+	fprintf(fid, "\n");
+}
+// data saving main function
+void save_data(double x1, double x2, double x3, double x4, double x5, double x6)
+{
+	fprintf(fid, "%f %f %f", x1, x2, x3);
+	fprintf(fid, "%f %f %f", x4, x5, x6);
+	fprintf(fid, "\n"); 
+}
+// call the data saving main function inside the main loop
+fid = fopen(path, "w");
+init_save_data();
+for ()
+{
+	save_data(x1, x2, x3, x4, x5, x6);
+}
+fclose(fid);
