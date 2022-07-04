039d8bea5849ef073e410852bfc0f169f71a3d63
chunmingYang <95987545+chunmingYang@users.noreply.github.com>
2022-07-03 20:17:59 -0500

    Update c_basic.c

--- c_basic.c
+++ c_basic.c
@@ -1,3 +1,20 @@
 /*
-  
+  how to pass variables within functions
 */
+
+double variable_1;
+double variable_2;
+
+double myfunction(double variable_1, double variable_2)
+{
+  double variable_3 = variable_1 + variable_2;
+  return variable_array[3] = {variable_1, variable_2, variable_3}; // how to return array
+}
+
+int main()
+{
+  double variable_1; // what is the difference here compare with the first definition
+  double variable_2;
+  result[3] = myfunction(variable_1, variable_2);
+    
+}
